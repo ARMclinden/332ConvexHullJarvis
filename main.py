3eb7e5c3bbdebb2751920138b94dfa514df4062a
Andrew McLinden <amclinden@mtech.edu>
2026-03-09 12:50:15 -0600

    day 1 changes no tests

--- main.py
+++ main.py
@@ -2,12 +2,30 @@ from typing import List, Tuple
 
 Point = Tuple[float, float]
 
-def convex_hull_jarvis(points: List[Point]) -> List[Point]:
-   pass
+def find_orientation(p, q, r):
 
+    if p == () or q == () or r == ():
+        return -1
+
+    fin = ((q[1]-p[1]) * (r[0]-q[0]) - (r[1] - q[1]) * (q[0] - p[0])) 
+
+    if fin == 0:    #collinear
+        return 0
+    elif fin > 0:   #
+        return 1
+    else:
+        return 2
+    
+def convex_hull_jarvis(points: List[Point]) -> List[Point]:
+
+    if points == []:
+        return -1
+    
+    for i in range(points):
+        
 
 if __name__ == "__main__":
-    points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)] 
+    points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)]
     hull = convex_hull_jarvis(points) 
     print("Convex Hull:", hull)
 
